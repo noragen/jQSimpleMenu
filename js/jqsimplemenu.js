03a2cd3407ee57c1d51d149ef7dc0d5d263564a8
noragen <noragen@gmx.net>
2015-05-20 14:49:06 +0200

    Update jqsimplemenu.js
    JQSimpleMenu with click-activation instead of mouseover. Analog to this close menu by click anywhere else and not with mouseleave.

--- js/jqsimplemenu.js
+++ js/jqsimplemenu.js
@@ -1,76 +1,94 @@
-﻿(function ($) {
-    $.fn.extend({
-        jqsimplemenu: function () {
-            return this.each(function () {
-                //add class .drop-down to all of the menus having drop-down items
-                var menu = $(this);
-                var timeoutInterval;
-                if (!menu.hasClass('menu')) menu.addClass('menu');
-                $("> li", menu).each(function () {
-                    if ($(this).find("ul:first").length > 0)
-                        $(this).addClass('pull-down');
-                });
+(function($) {
+	$.fn.extend({jqsimplemenu : function() {
+			return this.each(function() {
+					debugger;
+					// add class .drop-down to all of the menus having drop-down items
+					var menu = $(this);
+					var timeoutInterval;
+					if (!menu.hasClass('menu'))
+						menu.addClass('menu');
+					$("> li", menu).each(function() {
+						if ($(this).find("ul:first").length > 0)
+							$(this).addClass('pull-down');
+					});
 
-                $("> li ul li ul", menu).each(function () {
-                    $(this).parent().addClass('right-menu');
-                });
-                $("li", menu).mouseenter(function () {
-                    var isTopLevel = false;
-                    //if its top level then add animation 
-                    isTopLevel = $(this).parent().attr('class') === 'menu';
-                    if (isTopLevel) {
-                        clearTimeout(timeoutInterval);
-                        var w = $(this).outerWidth();
-                        // if ($(this).hasClass('pull-down')) w += 10;
-                        var h = $(this).outerHeight();
-                        var box = $('<div/>').addClass('box');
-                        $('> li', menu).removeClass('selected');
-                        $('>li div.box', menu).remove();
-                        $('>li ul', menu).css('display', 'none').slideUp(0);
-                        $(this).prepend(box);
-                        $(this).addClass('selected');
-                        box.stop(true, false).animate({ width: w, height: h }, 100, function () {
-                            if ($(this).parent().find('ul:first').length == 0) {
-                                timeoutInterval = setTimeout(function () {
-                                    box.stop(true, false).animate({ height: '+=5' }, 300, function () {
-                                        box.parent().find('ul:first').css('display', 'block').css('top', box.height()).stop(true, false).slideDown(100);
-                                    });
-                                }, 10);
-                            }
-                            else {
+					$("> li ul li ul", menu).each(function() {
+						$(this).parent().addClass('right-menu');
+					});
+					$("li", menu).click(function(e) {
+						e.stopPropagation();
+						var isTopLevel = false;
+						// if its top level then add animation
+						isTopLevel = $(this).parent().attr('class') === 'menu';
+						if (isTopLevel) {
+							clearTimeout(timeoutInterval);
+							var w = $(this).outerWidth();
+							// if ($(this).hasClass('pull-down')) w += 10;
+							var h = $(this).outerHeight();
+							var box = $('<div/>').addClass('box');
+							$('> li', menu).removeClass('selected');
+							$('>li div.box', menu).remove();
+							$('>li ul', menu).css('display','none').slideUp(0);
+							$(this).prepend(box);
+							$(this).addClass('selected');
+							box.stop(true, false).animate({width : w, height : h}, 100, function() {
+								if ($(this).parent().find('ul:first').length == 0) {
+									timeoutInterval = setTimeout(function() {
+										box.stop(true, false).animate({height : '+=5'}, 300, function() {
+											box.parent().find('ul:first').css('display','block').css('top',box.height()).stop(true,false).slideDown(100);
+										});
+									}, 10);
+								} 
+								else {
 
-                                timeoutInterval = setTimeout(function () {
-                                    box.stop(true, false).animate({ height: '+=0' }, 0, function () {
-                                        box.parent().find('ul:first').css('display', 'block').css('top', box.height()).stop(true, false).slideDown(100);
-                                    });
-                                }, 10);
-                            }
-                        });
-                    }
-                    else {
-                        $(this).find('ul:first').css('display','block').stop(true, false).slideDown(100);
-                    }
+									timeoutInterval = setTimeout(function() {
+										box.stop(true,false).animate({height : '+=0'},0,function() {
+											box.parent().find('ul:first').css('display','block').css('top',box.height()).stop(true,false).slideDown(100);
+										});
+									},10);
+								}
+							});
+						}
+					});
+					$("li", menu).mouseenter(function() {
+						var isTopLevel = false;
+						// if its top level then add animation
+						isTopLevel = $(this).parent().attr('class') === 'menu';
+						if (!isTopLevel) {
+							thisInstance = this;
+							timeoutInterval = setTimeout(function() {
+								$(thisInstance).find('ul:first').css('display','block').stop(true,false).slideDown(100);
+							},500);
+						}
+						$( menu ).trigger( "focus" );
+					})
+					.mouseleave(function(e) {
+						if(timeoutInterval > 0){
+							clearTimeout(timeoutInterval);
+						}
+						isTopLevel = $(this).parent().attr('class') === 'menu';
+						if (isTopLevel) {
+							return;
+						}
+						$(this).find('ul').slideUp(100, function() {$(this).css('display', 'none');										});
+					});
 
-                }).mouseleave(function () {
-                    isTopLevel = $(this).parent().attr('class') === 'menu';
-                    if (isTopLevel) {
-                        $(this).parent().find('div.box').remove();
-                    }
-                    $(this).find('ul').slideUp(100, function () {
+					$('> li > ul li a', menu).hover(function() {
+								$(this).parent().addClass('menu-item-selected');
+					}, function() {
 
-                        $(this).css('display', 'none');
-                    });
-                });
+								$(this).parent().removeClass('menu-item-selected');
+							});
 
-                $('> li > ul li a', menu).hover(function () {
-                    $(this).parent().addClass('menu-item-selected');
-                }, function () {
-
-                    $(this).parent().removeClass('menu-item-selected');
-                });
-
-            });
-        }
-    });
+					$(menu).parents().click(
+						function(e) {
+							$( menu ).trigger( "focusout" );
+						}
+					);
+					$(menu).focusout(function(e) {
+						$(menu).find('ul').slideUp(100, function() {$(this).css('display', 'none');});
+					});
+				});
+			}
+		});
 })(jQuery);
-
